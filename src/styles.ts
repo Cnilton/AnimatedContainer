63541a20df1eefae8a9fade0730ccc6de1929fcf
cnilton <caioniltonlemosbarroso@gmail.com>
2023-11-04 23:52:26 -0300

    removing pre-existing style

--- src/styles.ts
+++ src/styles.ts
@@ -3,13 +3,5 @@ import {StyleSheet} from 'react-native';
 export const style = StyleSheet.create({
   animatedViewStyle: {
     flexShrink: 1,
-    borderRadius: 8,
-    elevation: 4,
-    zIndex: 1,
-    backgroundColor: 'white',
-    shadowColor: '#171717',
-    shadowOffset: {width: -2, height: 4},
-    shadowOpacity: 0.6,
-    shadowRadius: 3,
   },
 });
